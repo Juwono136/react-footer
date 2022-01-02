717c926c3b9808c4ea60923e4c887a7dfb659333
juwono136 <unosega@gmail.com>
2022-01-02 22:44:17 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterContainer } from "./containers/footer";
+
+function App() {
+  return (
+    <React.StrictMode>
+      <FooterContainer />
+    </React.StrictMode>
+  );
+}
+
+export default App;
